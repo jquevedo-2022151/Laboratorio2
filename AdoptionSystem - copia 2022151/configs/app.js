4b8cf0bb547fb3427ea46eea22a5fa449c157e51
citzep-2019528 <citzep-2019528@kinal.edu.gt>
2024-02-09 17:33:53 -0600

    Se agrego el laboratorio 2

---
+++ AdoptionSystem - copia 2022151/configs/app.js
@@ -0,0 +1,56 @@
+//Configuracion de express
+
+//Importaciones
+import express from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import { config } from 'dotenv'
+import userRoutes from '../src/user/user.routes.js'
+
+
+//Configuraciones
+
+const app = express() //Crear el servidor
+config()
+const port = process.env.PORT || 3200
+
+// Configurar el servidor de express
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors()) //Aceptar o denegar las solicitudes de diferentes orígenes (local, remoto) / políticas de acceso
+app.use(helmet())
+app.use(morgan('dev')) //logs de solicitudes al servidor HTTP
+
+//Declaración de rutas
+
+app.use(userRoutes)
+
+//levantar el servidor
+export const initServer  = ()=>{
+    app.listen(port)
+    console.log(`Server HTTP running in port ${port}`)
+}
+
+//console.log('Aquí vamos a configurar express')
+
+
+/*
+
+&&
+
+V F = F
+V V = V
+F V = F
+F F = F
+
+
+||
+
+V F = V
+F V = V
+V V = V
+F F = F
+
+*/
